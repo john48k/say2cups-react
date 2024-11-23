10373771a2f7d3be03dbc770aab6d9c4058e4e39
john48k⌖ <126651207+john48k@users.noreply.github.com>
2024-11-23 21:03:06 +0800

    Beat Page: Card Getting Created

--- src/components/beatsPage.jsx
+++ src/components/beatsPage.jsx
@@ -1,22 +1,62 @@
 import { Navbar } from "./NavBar";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function Beats() {
   return (
     <div className="hero">
       <Navbar page="Beats" />
-      <div className="aboutpage">
-        <form className="put-middle">
-          <label id="beats-title" htmlFor="title">
-            TITLE
-          </label>
-          <input type="text" name="title" id="beats-title-input-box" />
-
-          <label id="beats-price" htmlFor="price">
-            PRICE
-          </label>
-          <input type="number" name="price" id="beats-price-input-box" />
-          <button id="beats-save-btn">Save</button>
-        </form>
+      <div className="beat-align-cards">
+        <div className="beat-card-container text_flick_faster">
+          <img
+            src="https://c4.wallpaperflare.com/wallpaper/577/906/576/japanese-cars-jdm-nissan-s15-nissan-silvia-spec-wallpaper-preview.jpg"
+            alt=""
+          />
+          <div className="text_flick">
+            <p>Title: </p>
+            <p>Key: </p>
+            <p>Bpm:</p>
+            <p>Genre:</p>
+            <p>Prod:</p>
+            <button className="beats-price-button">
+              <ShoppingCartIcon />
+              $30.00
+            </button>
+          </div>
+        </div>
+        <div className="beat-card-container text_flick_faster">
+          <img
+            src="https://i.pinimg.com/736x/2b/f0/58/2bf0589187b658af1258ba7e23a115b8.jpg"
+            alt=""
+          />
+          <div className="text_flick">
+            <p>Title: </p>
+            <p>Key: </p>
+            <p>Bpm:</p>
+            <p>Genre:</p>
+            <p>Prod:</p>
+            <button className="beats-price-button">
+              <ShoppingCartIcon />
+              $30.00
+            </button>
+          </div>
+        </div>
+        <div className="beat-card-container text_flick_faster">
+          <img
+            src="https://i.pinimg.com/enabled_lo_mid/736x/2b/83/b8/2b83b8791bde1c9f9786edf41887c136.jpg"
+            alt=""
+          />
+          <div className="text_flick">
+            <p>Title: </p>
+            <p>Key: </p>
+            <p>Bpm:</p>
+            <p>Genre:</p>
+            <p>Prod:</p>
+            <button className="beats-price-button">
+              <ShoppingCartIcon />
+              $30.00
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
